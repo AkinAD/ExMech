465bf658b98a6d20a1258cf8b7e284d9486d4bde
philipn7 <philipn7@gmail.com>
2018-01-21 20:52:54 -0500

    Created methods to get selected file and close frame.
    Modified ToyAuthoring to run the frame and launch the scenario with the
    selected file.

--- Enamel/src/enamel/Mainframe.java
+++ Enamel/src/enamel/Mainframe.java
@@ -16,6 +16,7 @@ import javax.swing.filechooser.FileSystemView;
 public class Mainframe {
 	
 	static JFrame Main_Frame;
+	File selectedFile;
 	
 	public Mainframe() {
 		
@@ -40,14 +41,14 @@ public class Mainframe {
 		
 		Main_Frame.add(Main_Panel);
 		Main_Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+	
 	}
 	
 	public void fileDialog() {
 
 		//This panel runs when you hit the "Read File Name" button
 		
-		JFileChooser File_Chooser = new JFileChooser();
+		JFileChooser File_Chooser = new JFileChooser("FactoryScenarios/");
 		File_Chooser.setDialogTitle("VoiceOver Demo");
 		File_Chooser.getAccessibleContext().setAccessibleDescription("Displays list of system files");
 		File_Chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
@@ -55,10 +56,23 @@ public class Mainframe {
 		int returnVal = File_Chooser.showDialog(null, "Read Name");
 		
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			System.out.println(File_Chooser.getSelectedFile().getName());
+			System.out.println("File chosen was: " + File_Chooser.getSelectedFile().getName());
+			selectedFile = File_Chooser.getSelectedFile();
+			
+			
+			
 			//To get the name of the file for voice over, use this "File_Chooser.getSelectedFile().getName()"
 		}
 	
+	}
+	
+	public File getSelectedFile() {
+		return selectedFile;
+	}
+	
+	public void close() {
+		Main_Frame.setVisible(false);
+		Main_Frame.dispose();
 	}
 
 	public static void main(String[] args) {
--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -1,9 +1,26 @@
 package enamel;
 
+import java.io.File;
+
 public class ToyAuthoring {
 
-    public static void main(String[] args) { 	  	
+    public static void main(String[] args) {
+    		Mainframe obj = new Mainframe();
+    		File file;
+    		
+    		//Proceeds only after file has been chosen.
+    		boolean repeat = true;
+    		while(repeat) {
+    			file = obj.getSelectedFile();
+    			if(file != null) {
+    				repeat = false;
+    			}
+    		}
+    		file = obj.getSelectedFile();
+    		obj.close();
+    		
+    		//start the Scenario Parser with chosen file.
     	    ScenarioParser s = new ScenarioParser(true);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
+    	    s.setScenarioFile(file.getAbsolutePath());
     }
 }
