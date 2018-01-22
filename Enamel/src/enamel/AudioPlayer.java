addcbfce9ad603831c34d14743a1d642122ad572
philipn7 <philipn7@gmail.com>
2018-01-22 12:18:15 -0500

    Reverted back to original.

--- Enamel/src/enamel/AudioPlayer.java
+++ Enamel/src/enamel/AudioPlayer.java
@@ -1,23 +1,16 @@
 package enamel;
-//IS this even a conflict?
-//this is a conflict!
-// conflict testing!
-//anothber conflict!
+
 public class AudioPlayer extends Player {
 	
 	
 	public AudioPlayer(int cellNum, int buttonNum)
 	{
-		System.out.println("idk what this will do i am juyst adding random code");
 	}
 
 	@Override
 	public void refresh() {
 		// TODO Auto-generated method stub
-		int yes = 0;
-		yes++;
-		System.out.println(yes);
-	int x;
+		
 	}
 
 	@Override
