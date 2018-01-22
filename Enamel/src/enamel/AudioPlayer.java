3a70c9a1f2bb0d6141d8ddea5634ed4cd72f1b08
AkinAD <mrspence004@gmail.com>
2018-01-22 12:01:04 -0500

    idek, test

--- Enamel/src/enamel/AudioPlayer.java
+++ Enamel/src/enamel/AudioPlayer.java
@@ -1,16 +1,20 @@
 package enamel;
-
+//IS this even a conflict?
 public class AudioPlayer extends Player {
 	
 	
 	public AudioPlayer(int cellNum, int buttonNum)
 	{
+		System.out.println("idk what this will do i am juyst adding random code");
 	}
 
 	@Override
 	public void refresh() {
 		// TODO Auto-generated method stub
-		
+		int yes = 0;
+		yes++;
+		System.out.println(yes);
+	int x;
 	}
 
 	@Override
