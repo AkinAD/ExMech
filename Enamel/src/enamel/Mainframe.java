dbd307c62d3188d4cf6797124782a5cd1b490d2a
Asam Malik <asammalik11@gmail.com>
2018-01-28 00:16:43 -0500

    New GUI for MainFrame with textbox and fileMenu
    Changed the dimensions of the GUI to fit the text box, file menu, and an
    area for buttons. Added a file menu with "open", "save" and "clear" menu
    items. You can open and save text files by using the respected menu
    items and clear the text in textArea by pressing "clear". The "Sample"
    button is a temp button that will be replaced by other buttons in the
    future. When the button is pressed, a text of "Sample Text" is placed in
    a new line. This action can be used later on when other buttons such as
    "delay" are added. I only changed the variable name in the TopAuthoring
    class.

--- Enamel/src/enamel/Mainframe.java
+++ Enamel/src/enamel/Mainframe.java
@@ -1,85 +1,155 @@
 package enamel;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.filechooser.FileSystemView;
+import javax.swing.text.BadLocationException;
 
 public class Mainframe {
 	
-	static JFrame Main_Frame;
-	File selectedFile;
+	public JFrame frmAuthoringApp;
+	private JTextArea textArea = new JTextArea();
+	private File selectedFile;
 	
 	public Mainframe() {
+		frmAuthoringApp = new JFrame();
+		frmAuthoringApp.setTitle("Authoring App");
+		frmAuthoringApp.setBounds(100, 100, 450, 300);
+		frmAuthoringApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmAuthoringApp.getContentPane().setLayout(null);
 		
-		//This is the main dialogue that shows when you hit run. 
-
-		Main_Frame = new JFrame("Scenario Picker"); //Akin made  change here to test nvda
-		Main_Frame.setVisible(true);
-		Main_Frame.setSize(500, 200);
-
-		JPanel Main_Panel = new JPanel();
-		Main_Panel.getAccessibleContext().setAccessibleDescription("Scenario file picker");
-		JButton File_Dialog = new JButton("Read File Name");
-
-		File_Dialog.addActionListener(new ActionListener() {
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(6, 19, 320, 220);
+		frmAuthoringApp.getContentPane().add(scrollPane);
+		
+		scrollPane.setColumnHeaderView(textArea);
+		
+		//Sample button: Adds "Sample Text" to the text field.
+		JButton btnSample = new JButton("Sample");
+		btnSample.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				fileDialog();
+				if (textArea.getText().equals("")) {
+					textArea.append("Sample Text");
+				} else {
+					int caretOffset;
+					int lineNumber;
+					int startOffset;
+					int endOffset;
+					try {
+						caretOffset = textArea.getCaretPosition();
+						lineNumber = textArea.getLineOfOffset(caretOffset);
+						startOffset = textArea.getLineStartOffset(lineNumber);
+						endOffset = textArea.getLineEndOffset(lineNumber);
+						if (startOffset == endOffset) {
+							textArea.replaceRange("Sample Text", startOffset, endOffset);
+						} else {
+							textArea.append("\n" + "Sample Text");
+						}
+					} catch (BadLocationException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		btnSample.setBounds(327, 19, 117, 29);
+		frmAuthoringApp.getContentPane().add(btnSample);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmAuthoringApp.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		mntmOpen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.setText("");
+				JFileChooser open = new JFileChooser("FactoryScenarios/");
+				int retrunVal = open.showOpenDialog(frmAuthoringApp);
+				if (retrunVal == JFileChooser.APPROVE_OPTION) {
+					try {
+						Scanner sc = new Scanner(new FileReader(open.getSelectedFile().getPath()));
+						selectedFile = open.getSelectedFile();
+						while (sc.hasNext()) {
+							textArea.append(sc.nextLine() + "\n");
+						}
+					} catch (FileNotFoundException e1) {
+						JOptionPane.showMessageDialog(null, e);
+					}
+				}
+			}
+		});
+		mnFile.add(mntmOpen);
+		
+		JMenuItem mntmClear = new JMenuItem("Clear");
+		mntmClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textArea.setText("");
 			}
 		});
 		
-		
-		Main_Panel.add(File_Dialog);
-		
-		Main_Frame.add(Main_Panel);
-		Main_Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mntmSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser save = new JFileChooser("FactoryScenarios/");
+				int retrunVal = save.showSaveDialog(frmAuthoringApp);
+				if (retrunVal == JFileChooser.APPROVE_OPTION) {
+					try {
+						BufferedWriter bf = new BufferedWriter(new FileWriter(save.getSelectedFile().getPath()));
+						bf.write(textArea.getText());
+						bf.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		mnFile.add(mntmSave);
+		mnFile.add(mntmClear);
 	}
-	
-	public void fileDialog() {
 
-		//This panel runs when you hit the "Read File Name" button
-		
-		JFileChooser File_Chooser = new JFileChooser("FactoryScenarios/");
-		File_Chooser.setDialogTitle("VoiceOver Demo");
-		File_Chooser.getAccessibleContext().setAccessibleDescription("Displays list of system files");
-		File_Chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-
-		int returnVal = File_Chooser.showDialog(null, "Read Name");
-		
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			System.out.println("File chosen was: " + File_Chooser.getSelectedFile().getName());
-			selectedFile = File_Chooser.getSelectedFile();
-			
-			
-			
-			//To get the name of the file for voice over, use this "File_Chooser.getSelectedFile().getName()"
-		}
-	
-	}
-	
 	public File getSelectedFile() {
 		return selectedFile;
 	}
-	
+
 	public void close() {
-		Main_Frame.setVisible(false);
-		Main_Frame.dispose();
+		frmAuthoringApp.setVisible(false);
+		frmAuthoringApp.dispose();
 	}
-
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Mainframe obj = new Mainframe();
-
+		Mainframe window = new Mainframe();
+		window.frmAuthoringApp.setVisible(true);
 	}
-
 }
