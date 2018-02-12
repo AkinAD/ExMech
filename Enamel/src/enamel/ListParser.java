c01d8e4f4e006c9e0d30fece0b5d55f514584835
AkinAD <mrspence004@gmail.com>
2018-02-12 14:18:16 -0500

    more

--- Enamel/src/enamel/ListParser.java
+++ Enamel/src/enamel/ListParser.java
@@ -43,19 +43,37 @@ public class ListParser {
 
     			jKey = "/~user-input";
     			sb.append(jKey);
-    			for(ArrayList<Node> p :s.buttons)
-    			{
-    				for(int i=0; i< p.size(); i++)
+    			//Now inside the Junction 
+    				for(ArrayList<Node> p :s.buttons)
+    				{ //For all the list of nodes in Junction 
+    					for(int i=0; i< p.size(); i++) //for all the 'nodes' contained in Junction
+    					{     aList.junctionGoto(i);   			//go to specific junction, execute a sequence of tasks
+    						for (Node t : aList.currentList)	
+    						{ 
+    							if(key == "#BUTTON" )
+    							{	jKey = "/~";
+    								jData = t.getData();
+    								sb.append(jKey + jData);  //append List name 
+    							}    		
+    							else if (key == "#TEXT")
+    							{
+    								sb.append(jData); //assumes it is simply text
+    							}
+    							else
+    				    		{
+    				    			sb.append(jKey + jData);    // this code assumes that every node is in the proper order readable by the braile cell
+    				    									// and simply places txt as they would be in the array list
+    				    		}
+    							//add /~nextt
+    							// now create the other parts of the scenario post junction
     			
-	    		{
-    			
-    			aList.junctionGoto(i);
-    			// now create the other parts of the scenario post junction
-    			
-	    		}
-    				
-    			}
-    		}
+    						}
+    					}
+    				//For loop iterating through separate new junction nodes ends here
+    				}
+    	
+    		//Junction code ends here
+    				}
     		else
     		{
     			sb.append(key + data);    // this code assumes that every node is in the proper order readable by the braile cell
